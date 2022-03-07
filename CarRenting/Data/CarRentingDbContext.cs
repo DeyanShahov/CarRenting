ce96c75ab0d77e1f7c0194c114daa77078283e67
redfoxbg <redfoxbg@gmail.com>
2022-03-07 15:39:03 +0200

    Add Car class

---
+++ CarRenting/Data/CarRentingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CarRenting.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRenting.Data
+{
+    public class CarRentingDbContext : IdentityDbContext
+    {
+        public CarRentingDbContext(DbContextOptions<CarRentingDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; init; }
+    }
+}
---
+++ CarRenting/Data/DataConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRenting.Data
+{
+    public class DataConstants
+    {
+        public const int CarBrandMaxLength = 20;
+        public const int CarModelMaxLength = 30;
+        public const int CarDescriptionMaxLength = 400;
+    }
+}
