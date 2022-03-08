01f145f0fe0ef4ae0234d7975022a2959d9fbf68
redfoxbg <redfoxbg@gmail.com>
2022-03-08 20:45:55 +0200

    Implement adding of new Car

---
+++ CarRenting/Controllers/CarsController.cs
@@ -0,0 +1,68 @@
+﻿using CarRenting.Data;
+using CarRenting.Data.Models;
+using CarRenting.Models.Cars;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRenting.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly CarRentingDbContext data;
+
+        public CarsController(CarRentingDbContext data)
+        {
+            this.data = data;
+        }
+        //public IActionResult Add() => View();
+        public IActionResult Add() => View(new AddCarFormModel
+        {
+            Categories = this.GetCarCategories()
+        });
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel carModel)
+        {
+            if (!data.Categories.Any(c => c.Id == carModel.CategoryId))
+            {
+                ModelState.AddModelError(nameof(carModel.CategoryId), "Category");
+            }
+
+
+            if (!ModelState.IsValid)
+            {
+                carModel.Categories = this.GetCarCategories();
+
+                return View(carModel);  
+            }
+
+
+            var car = new Car
+            {
+                Brand = carModel.Brand,
+                Model = carModel.Model,
+                Description = carModel.Description,
+                ImageUrl = carModel.ImageUrl,
+                Year = carModel.Year,
+                CategoryId = carModel.CategoryId
+            };
+
+            data.Cars.Add(car);
+
+            data.SaveChanges();
+            
+            return RedirectToAction("Index", "Home");        
+        }
+
+        private IEnumerable<CarCategoryViewModel> GetCarCategories()
+        {
+            return this.data
+                    .Categories
+                    .Select(c => new CarCategoryViewModel
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                    })
+                    .ToList();
+        }
+    }
+}
---
+++ CarRenting/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static CarRenting.Data.DataConstants;
+
+namespace CarRenting.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        [Required]
+        [StringLength(CarBrandMaxLength, MinimumLength = CarBrandMinLength, ErrorMessage = "{0} must be between {2} and {1}")]
+        public string Brand { get; init; }
+
+        [Required]
+        [StringLength(CarModelMaxLength, MinimumLength = CarModelMinLength, ErrorMessage = "{0} must be between {2} and {1}")]
+        public string Model { get; init; }
+
+        [Required]
+        [StringLength(CarDescriptionMaxLength, MinimumLength = CarDescriptionMinLength, ErrorMessage = "{0} must be between {2} and {1}")]
+        public string Description { get; init; }
+
+        [Required]
+        [Url]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; init; }
+
+        [Range(CarYearMinValue, CarYearMaxValue)]
+        public int Year { get; init; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CarCategoryViewModel>? Categories { get; set; }
+    }
+}
