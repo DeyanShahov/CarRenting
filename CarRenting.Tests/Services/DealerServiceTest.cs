047a7109f7188c1d5d707224df5819273ce72dbb
redfoxbg <redfoxbg@gmail.com>
2022-04-04 20:58:25 +0300

    Some component test on service

---
+++ CarRenting.Tests/Services/DealerServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using CarRenting.Data;
+using CarRenting.Data.Models;
+using CarRenting.Services.Dealers;
+using CarRenting.Tests.Moq;
+using Xunit;
+
+namespace CarRenting.Tests.Services
+{
+    public class DealerServiceTest
+    {
+        Dealer dealer = new Dealer { Name = "Pesho", PhoneNumber = "088234455", UserId = "TestUserId" };
+
+        [Fact]
+        public void IsDealerShouldReturnTrueWhenUserIsDealer()
+        {
+            //Arange
+            var dealerService = GetDealerService(dealer);
+         
+            //Act
+            var result = dealerService.IsDealer(dealer.UserId);
+
+            //Assert
+            Assert.True(result);
+        }
+
+      
+
+        [Fact]
+        public void IsDealerShouldReturnFalseWhenUserIsNotDealer()
+        {
+            //Arange
+            var dealerService = GetDealerService(dealer);
+
+            //Act
+            var result = dealerService.IsDealer("AnotherId");
+
+            //Assert
+            Assert.False(result);
+        }
+
+
+        private IDealerService GetDealerService(Dealer dealer)
+        {            
+            var data = DatabaseMock.Instance;
+
+            data.Dealers.Add(dealer);
+
+            data.SaveChanges();
+
+            return new DealerService(data);
+        }
+    }
+}
