de4895293ee1d090b6e03d1d878a4e49ad70bfe6
redfoxbg <redfoxbg@gmail.com>
2022-04-04 22:33:43 +0300

    Add Integration test

--- CarRenting.Tests/Controller/HomeControllerTest.cs
+++ CarRenting.Tests/Controller/HomeControllerTest.cs
@@ -1,11 +1,57 @@
-﻿using CarRenting.Controllers;
+﻿using AutoMapper;
+using CarRenting.Controllers;
+using CarRenting.Data.Models;
+using CarRenting.Models.Home;
+using CarRenting.Services.Cars;
+using CarRenting.Services.Statistics;
+using CarRenting.Tests.Moq;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Linq;
 using Xunit;
 
 namespace CarRenting.Tests.Controller
 {
     public class HomeControllerTest
     {
+        [Fact]
+        public void IndexShouldReturnViewWithCorrectModel()
+        {
+            //Arange
+            var data = DatabaseMock.Instance;
+            var mapper = MapperMock.Instance;
+
+            //var cars = Enumerable.Range(0, 10).Select(i => new Mock<Car>());
+            var cars = Enumerable.Range(0, 10).Select(i => new Car { Brand = "fff", Description ="fffff", ImageUrl="ffff", Model="gggg"});
+
+            data.Cars.AddRange(cars);
+            data.Users.Add(new User { Email="ffff", FullName="ffffff"});
+
+            data.SaveChanges();
+
+            var carService = new CarService(data, mapper);
+            var statisticsService = new StatisticsService(data);
+
+            var homeController = new HomeController(statisticsService, carService);
+
+            //Act
+            var result = homeController.Index();
+
+            //Assert
+            Assert.NotNull(result);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+
+            var indexViewModel = Assert.IsType<IndexViewModel>(model);
+
+            Assert.Equal(3, indexViewModel.Cars.Count);   
+            Assert.Equal(10, indexViewModel.TotalCars);   
+            Assert.Equal(1, indexViewModel.TotalUsers);
+        }
+
+
         [Fact]
         public void ErrorShouldReturnView()
         {
---
+++ CarRenting.Tests/Moq/MapperMock.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CarRenting.Infrastructure;
+
+namespace CarRenting.Tests.Moq
+{
+    public static class MapperMock
+    {
+        public static IMapper Instance
+        {
+            get
+            {
+                var mapperConfiguration = new MapperConfiguration(config =>
+                {
+                    config.AddProfile<MappingProfile>();
+                });
+
+                return new Mapper(mapperConfiguration);
+            }
+        }
+    }
+}
