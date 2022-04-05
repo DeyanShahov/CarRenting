f42346d4540eb20e4d8de2f8ba1a115560b1e70e
redfoxbg <redfoxbg@gmail.com>
2022-04-05 21:05:07 +0300

    Add cache on lastestCars carosel view

--- CarRenting/Controllers/HomeController.cs
+++ CarRenting/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using CarRenting.Models.Home;
 using CarRenting.Services.Cars;
 using CarRenting.Services.Statistics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 
 namespace CarRenting.Controllers
@@ -14,18 +15,32 @@ namespace CarRenting.Controllers
     {
         private readonly ICarSevice carsService;
         private readonly IStatisticsService statistics;
+        private readonly IMemoryCache cache;
 
-        public HomeController(IStatisticsService statistics, ICarSevice carsService)
+        public HomeController(IStatisticsService statistics, ICarSevice carsService, IMemoryCache cache)
         {
 
             this.statistics = statistics;
             this.carsService = carsService;
+            this.cache = cache;
         }
 
         public IActionResult Index()
         {
-            var latestCars = carsService.Latest().ToList();
+            const string latestCarsCacheKey = "LatestCarsCaheKey";
 
+            var latestCars = cache.Get<List<LatestCarServiceModel>>(latestCarsCacheKey);
+
+            if (latestCars == null)
+            {
+                latestCars = carsService.Latest().ToList();
+
+                var cacheOption = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(5));
+
+                this.cache.Set(latestCarsCacheKey, latestCars, cacheOption);
+            }
+          
             var totalStatistics = statistics.Total();
 
             return View(new IndexViewModel
